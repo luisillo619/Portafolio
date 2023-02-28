240a05b388fe5998feed21da17dd65d5a748a981
luisillo619 <luiscarlosrangel619@gmail.com>
2023-02-28 01:35:04 -0600

    cambio colores

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -29,7 +29,7 @@ export default function Projects() {
       className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Projects
+        Proyectos
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
@@ -68,7 +68,7 @@ export default function Projects() {
         })}
       </div>
 
-      <div className="w-full absolute top-[30%] bg-[#F7AB01]/10 left-0 h-[500px] -skew-y-12"></div>
+      <div className="w-full absolute top-[30%] bg-[#ff0000]/40 left-0 h-[500px] -skew-y-12"></div>
     </motion.div>
   );
 }
