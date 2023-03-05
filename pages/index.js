ff90597709bc4ce5f8f0877bf9efd2a0e3c3b7ba
luisillo619 <luiscarlosrangel619@gmail.com>
2023-03-04 21:26:33 -0600

    bugs resposive

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
         <ContactMe />
       </section>
       <Link href={"#hero"}>
-        <footer className="sticky bottom-[3.5%] w-full cursor-pointer p-5">
+        <footer className="sticky bottom-[3.5%] w-full cursor-pointer p-6">
           <div className="flex items-center justify-center">
             <img
               className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
